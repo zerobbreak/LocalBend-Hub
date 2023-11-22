51f8d3392adf92c05e3381c8ba5252ebb1b424ea
zerobbreak <uthacker244@outlook.com>
2023-11-22 09:18:28 +0200

    Hackathon Submission

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import WelcomeScreen from "./screens/WelcomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import HomeScreen from "./screens/HomeScreen";
@@ -10,16 +9,67 @@ import SearchScreen from "./screens/SearchScreen";
 import OverviewScreen from "./screens/OverviewScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import Results from "./screens/Results";
+import { Entypo } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 
-const BottomTabBar = () =>{
+const BottomTabBar = () => {
   const Tab = createBottomTabNavigator();
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen}/>
-      <Tab.Screen name="Discover" component={DiscoveryScreen}/>
-      <Tab.Screen name="Search" component={SearchScreen}/>
-      <Tab.Screen name="Overview" component={OverviewScreen}/>
-      <Tab.Screen name="Profile" component={ProfileScreen}/>
+      <Tab.Screen name="Home" component={HomeScreen} options={{
+        headerShown: false, tabBarLabel: "Home", tabBarLabelStyle: { color: '#000' }, tabBarIcon: ({ focused }) =>
+          focused ? (
+            <Entypo name="home" size={24} color="black" />
+          ) : (
+            <AntDesign name="home" size={24} color="black" />
+          ),
+      }}
+      />
+
+      <Tab.Screen name="Discover" component={DiscoveryScreen} options={{
+        headerShown: false, tabBarLabel: "Home", tabBarLabelStyle: { color: '#000' }, tabBarIcon: ({ focused }) =>
+          focused ? (
+            <Entypo name="home" size={24} color="black" />
+          ) : (
+            <AntDesign name="home" size={24} color="black" />
+          ),
+      }} 
+      />
+
+      <Tab.Screen name="Search" component={SearchScreen}
+        options={{
+          headerShown: false, tabBarLabel: "Home", tabBarLabelStyle: { color: '#000' }, tabBarIcon: ({ focused }) =>
+            focused ? (
+              <FontAwesome name="search" size={24} color="black" />
+            ) : (
+              <Ionicons name="search-outline" size={24} color="black" />
+            ),
+        }}
+      />
+
+      <Tab.Screen name="Overview" component={OverviewScreen}
+        options={{
+          headerShown: false, tabBarLabel: "Home", tabBarLabelStyle: { color: '#000' }, tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Entypo name="home" size={24} color="black" />
+            ) : (
+              <AntDesign name="home" size={24} color="black" />
+            ),
+        }}
+      />
+
+      <Tab.Screen name="Profile" component={ProfileScreen}
+        options={{
+          headerShown: false, tabBarLabel: "Home", tabBarLabelStyle: { color: '#000' }, tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Ionicons name="person" size={24} color="black" />
+            ) : (
+              <Ionicons name="person-outline" size={24} color="black" />
+            ),
+        }}
+      />
     </Tab.Navigator>
   )
 }
@@ -29,11 +79,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Welcome" component={WelcomeScreen}/>
-        <Stack.Screen name="Login" component={LoginScreen}/>
-        <Stack.Screen name="Sign Up" component={RegisterScreen}/>
-        <Stack.Screen name="Main" component={BottomTabBar}/>
-        <Stack.Screen name="Search Results" component={Results}/>
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Sign Up" component={RegisterScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Main" component={BottomTabBar} options={{ headerShown: false }} />
+        <Stack.Screen name="Search Results" component={Results} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,43 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, Pressable, Image, ScrollView, KeyboardAvoidingView } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 
 const LoginScreen = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "white", alignItems: 'center' }}>
+      <View style={{ marginTop: 50 }}>
+        <Image
+          style={{ width: 280, height: 180, resizeMode: 'contain', borderRadius: 5 }}
+          source={require("../imgs/Community.jpg")}
+        />
+      </View>
+
+      <KeyboardAvoidingView>
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+          <Text style={{ fontSize: 17, fontWeight: 'bold', marginTop: 20 }}>Login</Text>
+          <Text style={{ fontSize: 14, fontWeight: '400', marginTop: 10 }}>Welcome to your community</Text>
+        </View>
+
+        <View style={{ marginTop: 40 }}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', gap: 5, borderColor: "#000", borderWidth: 1, paddingVertical: 5, borderRadius: 5 }}>
+            <MaterialCommunityIcons style={{ marginLeft: 8 }} name="email" size={24} color="gray" />
+            <TextInput value={email} onChangeText={(text) => setEmail(text)} placeholderTextColor={"grey"} style={{ color: 'gray', marginVertical: 10, width: 270, fontSize: email ? 16 : 16 }} placeholder='enter your email' />
+          </View>
+
+          <View style={{ marginTop: 30, }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center', gap: 5, borderColor: "#000", borderWidth: 1, paddingVertical: 5, borderRadius: 5 }}>
+              <AntDesign style={{ marginLeft: 8 }} name="lock" size={24} color="black" />
+              <TextInput value={password} onChangeText={(text) => setPassword(text)} placeholderTextColor={"grey"} style={{ color: 'gray', marginVertical: 10, width: 270, fontSize: password ? 16 : 16 }} placeholder='enter your password' />
+            </View>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   )
 }
 
